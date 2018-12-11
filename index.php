330a08b2eec486d58a185a52ac6fc07fb0b517ff
Christan RCSG <christian.floppy@gmail.com>
2018-12-10 21:44:12 -0600

    Esqueleto
    Primera pagina Web del Esqueleto del sitio web

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PROESCU Inicio</title>
+	<link rel="stylesheet" href="style/style.css">
+</head>
+<body>
+	<header>
+		<img class="imgFondo" src="image/homeBackground-min.jpg">
+	</header>
+	<section>
+		
+	</section>
+	<section>
+		
+	</section>
+	<footer>
+		© 2018 Programa Escolar de Cursos.  Todos los derechos reservados.
+	</footer>
+</body>
+</html>
