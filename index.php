88a7ff3700f10513ae8051885b57e3fa296feb21
Christan RCSG <christian.floppy@gmail.com>
2018-12-13 19:22:18 -0600

    Nosotros
    Se termino el nosotros con informacion respecto a lo que es el sitio

--- index.php
+++ index.php
@@ -3,24 +3,84 @@
 <head>
 	<title>PROESCU Inicio</title>
 	<link rel="stylesheet" href="style/style.css">
+	<style>
+@import url('https://fonts.googleapis.com/css?family=Love+Ya+Like+A+Sister');
+</style>
 </head>
 <body>
 	<header class="header">
-		<div class="backImg">
-			<div class="imgFondo">
-				<img class="imgLogo" src="image/logo-min.png" alt="Logo PROESCU">
+		<div class="ContTop1 colorDegradado">
+			<div class="backImg">
+				<div class="imgFondo">
+					<img class="imgLogo" src="image/logo-min.png" alt="Logo PROESCU">
+					<div class="ContMenu">
+						<a href="#" class="boton">¿QUIENES SOMOS?</a>
+						<a href="#" class="boton">CURSOS</a>
+						<a href="#" class="boton">NOTICIAS</a>
+						<a href="#" class="boton">CONTACTO</a>
+					</div>
+				</div>
 			</div>
-			
+		</div>
+		<div class="ContTop2 colorDegradado">
+			<h1 class="titleHome">¡Por el gusto de aprender y enseñar!</h1>
+			<h2 class="SubTitleHome">Un concepto de estudiantes para estudiantes</h2>
+			<a class="btnContactar" href="#"><button class="btnHome">CONTACTO</button></a>
 		</div>
 	</header>
 	<section>
-		
+		<!--seccion DE BANER PARA INVITACION-->
+		<article class="invita">
+			<div class="ContInv1">
+				<div class="cajaInv">
+					<h1 class="titleInv">¡Quiero aprender a programar!</h1>
+					<div class="contImgInv">
+						<img class="iconInv" src="image/icons/C.png" alt="Lenguaje C">
+						<img class="iconInv" src="image/icons/html5.png" alt="HTML">
+						<img class="iconInv" src="image/icons/css-3.png" alt="Hojas de Estilo">
+						<img class="iconInv" src="image/icons/java.png" alt="Lenguaje Java">
+					</div>
+					<p class="descInvita">
+						En nuestro catálogo de cursos podrás elegir e inscribirte a los que desees, se constante, pues están abiertos a todo público y depende cada tutor el cupo. Inscríbete y aprende algo nuevo cada día.
+					</p>
+					<a class="btnContactar" href="#"><button class="btnHome">SABER MÁS</button></a>
+				</div>
+			</div>
+			<div class="ContInv2">
+				<div class="cajaInv">
+					<h1 class="titleInv">¡Quiero contribur siendo tutor!</h1>
+					<div class="contImgInv">
+						<img class="iconInv" src="image/icons/mortarboard.png" alt="Lenguaje C">
+					</div>					
+					<p class="descInvita">
+						Compartir tu conocimiento es algo generoso, inscríbete como tutor y arma tu clase, se asesor de compañeros que buscan el mismo objetivo:  <strong>aprender</strong>.
+					</p>
+					<a class="btnContactar" href="#"><button class="btnHome">SABER MÁS</button></a>
+				</div>
+			</div>
+		</article>
+		<article class="aboutUs">
+			<div class="contAbout">
+				<p class="textPizarra">PROESCU es una comunidad de alumnos para alumnos donde el apoyo mutuo es el motor de la plataforma. Siempre hay temas por aprender y muchos están dispuestos a asesorar, o bien, tu deseas mostrarle a la comunidad tu conocimiento. Inscríbete como alumno y toma los cursos que desees en vivo, inscríbete como tutor y programa una o varias sesiones de tutoría o cursos.</p>
+			</div>
+			<div class="contimgPro">
+				<img class="prograImg" src="image/programas.png" alt="Lenguajes de programacion">
+			</div>
+		</article>
 	</section>
 	<section>
 		
 	</section>
+
+
+
 	<footer>
+		<p>
+			Hecho con <a href="https://www.facebook.com/jeniper.666"> &#128147; </a>Por <a href="https://www.facebook.com/recsigua"> 	Christian RCSG </a> para ti cariño mío.
+		</p>
+		<p>
 		© 2018 Programa Escolar de Cursos.  Todos los derechos reservados.
+		</p>
 	</footer>
 </body>
 </html>
