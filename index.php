e86c3aba54401ccb7079311cbe5af75eb265a6bb
Christan RCSG <christian.floppy@gmail.com>
2018-12-13 22:52:50 -0600

    Liberar
    EL sitio esta disponible

--- index.php
+++ index.php
@@ -7,25 +7,33 @@
 @import url('https://fonts.googleapis.com/css?family=Love+Ya+Like+A+Sister');
 </style>
 </head>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v3.2&appId=133418343358789&autoLogAppEvents=1';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 <body>
 	<header class="header">
-		<div class="ContTop1 colorDegradado">
+		<div class="ContTop1 colorSemiDegradado">
 			<div class="backImg">
 				<div class="imgFondo">
 					<img class="imgLogo" src="image/logo-min.png" alt="Logo PROESCU">
 					<div class="ContMenu">
-						<a href="#" class="boton">¿QUIENES SOMOS?</a>
-						<a href="#" class="boton">CURSOS</a>
-						<a href="#" class="boton">NOTICIAS</a>
-						<a href="#" class="boton">CONTACTO</a>
+						<a href="#nosotros" class="boton">¿QUIENES SOMOS?</a>
+						<a href="#cursos" class="boton">CURSOS</a>
+						<a href="#noticias" class="boton">NOTICIAS</a>
+						<a href="#contacto" class="boton">CONTACTO</a>
 					</div>
 				</div>
 			</div>
 		</div>
-		<div class="ContTop2 colorDegradado">
+		<div class="ContTop2 colorSemiDegradado">
 			<h1 class="titleHome">¡Por el gusto de aprender y enseñar!</h1>
 			<h2 class="SubTitleHome">Un concepto de estudiantes para estudiantes</h2>
-			<a class="btnContactar" href="#"><button class="btnHome">CONTACTO</button></a>
+			<a class="btnContactar" href="#contacto"><button class="btnHome">CONTACTO</button></a>
 		</div>
 	</header>
 	<section>
@@ -59,7 +67,7 @@
 				</div>
 			</div>
 		</article>
-		<article class="aboutUs">
+		<article id="nosotros" class="aboutUs">
 			<div class="contAbout">
 				<p class="textPizarra">PROESCU es una comunidad de alumnos para alumnos donde el apoyo mutuo es el motor de la plataforma. Siempre hay temas por aprender y muchos están dispuestos a asesorar, o bien, tu deseas mostrarle a la comunidad tu conocimiento. Inscríbete como alumno y toma los cursos que desees en vivo, inscríbete como tutor y programa una o varias sesiones de tutoría o cursos.</p>
 			</div>
@@ -68,19 +76,125 @@
 			</div>
 		</article>
 	</section>
-	<section>
-		
+	<section id="cursos">
+		<article class="cursos">
+			<div class="containerCatalog">
+				<div class="area-g0 ContCuentas txtAcor productosName">
+					<h1><strong>LENGUAJE C</strong></h1>
+					<img class="prod" src="image/icons/C.png">
+					<p class="txtDesCurso">
+						10 sesiones <br>
+						LUN, MIE, VIE <br>
+						20 hrs. <br><br>
+						INICIA: 17 DIC 2018 <br>
+						DISPONIBILIDAD: 6/10<br>
+						<br>
+						TUTOR: Christian RCGS <br>
+						COSTO: CURSO PILOTO <br>
+					</p>
+					<a class="btnContactar" href="#"><button class="btnAccesCurso">SABER MÁS</button></a>
+				</div>
+				<div class="area-g0 ContCuentas txtAcor productosName">
+					<h1>DISEÑO WEB</h1>
+					<img class="prod" src="image/icons/html5.png">
+					<p class="txtDesCurso">
+						10 sesiones <br>
+						LUN, MIE, VIE <br>
+						20 hrs. <br><br>
+						INICIA: 2 ENE 2019 <br>
+						DISPONIBILIDAD: 6/10<br>
+						<br>
+						TUTOR: Christian RCGS <br>
+						COSTO: CURSO PILOTO <br>
+					</p>
+					<a class="btnContactar" href="#"><button class="btnAccesCurso">SABER MÁS</button></a>
+				</div>
+			</div>
+		</article>	
 	</section>
+	<section id="noticias">
+		<article class="redesNotice">
+			<div class="colorSemiDegradado contRed">
+				<div class="rssCont colorDegradado">
+					<h1 class="noticeTitle">NOTICIAS</h1>
+					<div class="txtDataContacto">
+						<img class="imgRedes" src="image/icons/rss.png">
+						<p class="txtData">
+							13 DIC 18 <br>
+							<strong>Nuevo curso "Diseño Web</strong> - Chris RCGS <br>
+							Iniciamos curso de Diseño Web, inscríbete antes de que ganen tu lugar. Curso PILOTO, lo que indica que el costo del curso es de aportación voluntaria. Para inscribirte manda un correo a proescu.contacto@gmail.com con asunto: INSCRIPCION. Iniciamos el 2 de Enero de 2019.
+						</p>
+					</div>
 
+					<div class="txtDataContacto">
+						<img class="imgRedes" src="image/icons/rss.png">
+						<p class="txtData">
+							13 DIC 18 <br>
+							<strong>Nuevo curso "LENGUAJE C"</strong> - Chris RCGS <br>
+							Iniciamos curso de Programación en C, inscríbete antes de que ganen tu lugar. Curso PILOTO, lo que indica que el costo del curso es de aportación voluntaria. Para inscribirte manda un correo a proescu.contacto@gmail.com con asunto: INSCRIPCION. Iniciamos el 17 de Diciembre de 2018.
+						</p>
+					</div>
 
-
+					<div class="txtDataContacto">
+						<img class="imgRedes" src="image/icons/rss.png">
+						<p class="txtData">
+							6 DIC 18 <br>
+							<strong>A ti cariño mio</strong> - Chris RCGS <br>
+							Sin mucho detalle, este proyecto de lo dedico a ti.
+						</p>
+					</div>
+				</div>
+				<div class="contRedSocial">
+					<div class="fb-page" data-href="https://www.facebook.com/ReCSiGuaStuDios/" data-tabs="timeline" data-width="500" data-small-header="false" data-adapt-container-width="false" data-hide-cover="false" data-show-facepile="false"><blockquote cite="https://www.facebook.com/ReCSiGuaStuDios/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/ReCSiGuaStuDios/">ReÇSiGua-StuDios</a></blockquote></div>
+				</div>
+			</div>
+		</article>
+	</section>
+	<section id="contacto">
+		<div class="incripcion">
+			<h1 class="funcion">¿CÓMO FUNCIONA?</h1>
+			<div class="txtDataContacto">
+				<img class="imgInstrucciones" src="image/icons/select.png">
+				<p class="txtComo">Elige el curso que desees.</p>
+			</div>
+			<div class="txtDataContacto">
+				<img class="imgInstrucciones" src="image/icons/gmail.png">
+				<p class="txtComo">Manda un correo a para inscribirte con los siguientes datos: 
+					<br>• Nombre (Solo un nombre y un apellido)</ul>
+					<br>• Alias (como te gusta que te llamemos)</ul>
+					<br>• Correo (debe ser GMAIL)</ul>
+					<br>• A que curso deseas inscribirte (Pueden ser uno o más)</ul>
+					<br>• Tu alma mater</ul>
+					<br>• Carrera</ul>
+					<br>• Ocupación</ul>
+					</p>
+			</div>
+			<div class="txtDataContacto">
+				<img class="imgInstrucciones" src="image/icons/hangouts.png">
+				<p class="txtComo">Te agregaremos al grupo de GMAIL de video conferencia de Hangouts. Estate pendiente de las horas de sesión. Entra al curso desde tu cuenta de GMAIL</p>
+			</div>
+			<div class="txtDataContacto">
+				<img class="imgInstrucciones" src="image/icons/notepad.png">
+				<p class="txtComo">Toma notas y apuntes, habrá tareas y ejercicios que debes cumplir como compromiso. </p>
+			</div>
+			<div class="txtDataContacto">
+				<img class="imgInstrucciones" src="image/icons/question.png">
+				<p class="txtComo">Pregunta dudas y si tienes sugerencias estamos para ayudarte. Y si no lo sabemos, pues le preguntamos a San Google</p>
+			</div>
+			<div class="txtDataContacto">
+				<img class="imgInstrucciones" src="image/icons/love.png">
+				<p class="txtComo">No olvides darle Like a nuestra pagina de facebook, recomendarnos y estar atento a próximos cursos. </p>
+			</div>
+			<div class="txtDataContacto">
+				<img class="imgInstrucciones" src="image/icons/mortarboard.png">
+				<p class="txtComo">¿Quieres formar parte!, ¡sé un tutor también!, espera noticias de ello.  </p>
+			</div>
+		</div>
+	</section>
 	<footer>
-		<p>
-			Hecho con <a href="https://www.facebook.com/jeniper.666"> &#128147; </a>Por <a href="https://www.facebook.com/recsigua"> 	Christian RCSG </a> para ti cariño mío.
-		</p>
-		<p>
-		© 2018 Programa Escolar de Cursos.  Todos los derechos reservados.
-		</p>
+		<p>Hecho con <a href="https://www.facebook.com/jeniper.666"> &#128147; </a>Por <a href="https://www.facebook.com/recsigua"> 	Christian RCSG </a> para ti cariño mío.</p>
+		<p>© 2018 Programa Escolar de Cursos.  Todos los derechos reservados.</p>
+		<p>POLITICAS DE PRIVACIDAD</p>
 	</footer>
 </body>
 </html>
